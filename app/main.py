eada61c63172515f5939bdae2e89329121a93f0a
Jacob Sanzone <jacobsanz1@gmail.com>
2025-01-23 17:37:31 -0800

    added type command

--- app/main.py
+++ app/main.py
@@ -1,23 +1,35 @@
 import sys
+def cmdExit(code,_):
+    exitCode = int(code[0])
+    sys.exit(exitCode)
 
+    
+def cmdEcho(message,_):
+    print(message)
+
+def cmdType(command,commandDict):
+    try:
+        print(commandDict[command[0]] + " is a shell builtin")
+    except KeyError:
+        print(command[0] + ": not found")
 
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
+    commandDict = {
+        "exit": cmdExit,
+        "echo": cmdEcho,
+    }
 
     # Wait for user input
     while True:
         userInput = input()
         userTokens = userInput.split()
         if len(userTokens) != 0:
-            match userTokens[0]:
-                case "exit":
-                    exitCode = int(userTokens[1])
-                    sys.exit(exitCode)
-                case "echo":
-                    print(" ".join(userTokens[1:]))
-                case _:
-                    print(userInput + ": command not found")
+            try:
+                commandDict[userTokens[0]](userTokens[1:],commandDict)
+            except KeyError:
+                print(userInput + ": command not found")
         sys.stdout.write("$ ")
 
 
