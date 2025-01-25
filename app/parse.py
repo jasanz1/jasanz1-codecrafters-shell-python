acbc7a3b1e7ea7316fa069bbf6417bd0326ebc28
Jacob Sanzone <jacobsanz1@gmail.com>
2025-01-24 17:11:54 -0800

    rewrote parser

--- app/parse.py
+++ app/parse.py
@@ -33,51 +33,68 @@ class Token:
     
 def parse(userInput):
     userTokens = []
-    quoteStack = []
     tokenString = ""
-    escaped = False
-    for char in userInput:
-        if escaped:
-            debug.debug(f"escaped: {char}")
-            tokenString += char
-            escaped = False
-        else:
-            debug.debug(f"not escaped: {char}")
-            match char:
-                case '"':
-                    if len(quoteStack) > 0 and quoteStack[-1] == "'":
-                        tokenString += char
-                    elif len(quoteStack) == 0 or quoteStack[-1] == '"':
-                        quoteStack.pop()
-                        userTokens.append(Token(tokenType.doubleQuote, tokenString,False))
-                        tokenString = ""
-                    else:
-                        quoteStack.append('"')
-                case "'":
-                    if len(quoteStack) > 0 and quoteStack[-1] == "'":
-                        quoteStack.pop()
-                        userTokens.append(Token(tokenType.singleQuote, tokenString,False))
-                        tokenString = ""
-                    else:
-                        if len(quoteStack) == 0 or quoteStack[-1] != '"':
-                            quoteStack.append("'")
-                        else:
-                            tokenString += char
-                case '\\':
-                    if len(quoteStack) == 0 or quoteStack[-1] == '"':
-                        escaped = True
-                    else:
-                        tokenString += char
-                case ' ':
-                    if len(quoteStack) == 0:
-                        if len(tokenString) != 0:
-                            userTokens.append(Token(tokenType.string, tokenString, False))
-                        userTokens[-1].setFollowedByWhitespace(True)
-                        tokenString = ""
-                    else:
-                        tokenString += char
-                case _:
-                        tokenString += char
+    i = 0
+    while i < len(userInput):
+        char = userInput[i]
+        match char:
+            case '"':
+                doubleQuoteReturn,i = doubleQuote(userInput,i)
+                userTokens.append(doubleQuoteReturn)
+            case "'":
+                singleQuoteReturn,i = singleQuote(userInput,i)
+                userTokens.append(singleQuoteReturn)
+            case '\\':
+                escapedCharReturn,i = escapedChar(userInput,i)
+                tokenString += escapedCharReturn
+            case ' ':
+                if len(userTokens) != 0:
+                    userTokens[-1].setFollowedByWhitespace(True)
+                    if len(tokenString) != 0:
+                        userTokens.append(Token(tokenType.string, tokenString,False))
+                else:
+                    userTokens.append(Token(tokenType.whitespace, tokenString,True))
+                tokenString = ""
+            case _:
+                tokenString += char
+        i += 1
     if len(tokenString) != 0:
         userTokens.append(Token(tokenType.string, tokenString,False))
+    debug.debug(f"userTokens: {userTokens}")
     return userTokens
+
+
+def singleQuote(userInput,i):
+    tokenString = ""
+    i += 1
+    while i < len(userInput):
+        char = userInput[i]
+        if char == "'":
+            return (Token(tokenType.singleQuote, tokenString, False),i)
+        else:
+            tokenString += char
+            i += 1
+    return (Token(tokenType.singleQuote, tokenString, False),i)
+
+def escapedChar(userInput,i):
+    return (userInput[i+1],i+2)
+
+def doubleQuote(userInput,i):
+    tokenString = ""
+    i += 1
+    while i < len(userInput):
+        char = userInput[i]
+        debug.debug(f"tokenString: {userInput[i:]}")
+        match char:
+            case '"':
+                return (Token(tokenType.doubleQuote, tokenString, False),i)
+            case "'":
+                singleQuoteReturn,i = singleQuote(userInput,i)
+                tokenString += "'" + singleQuoteReturn.value + "'"
+            case '\\':
+                escapedCharReturn,i = escapedChar(userInput,i)
+                tokenString += escapedCharReturn
+            case _:
+                tokenString += char
+        i += 1
+    return (Token(tokenType.doubleQuote, tokenString, False),i)
